ea5baaa7067be705673f37c09a34e24b270699bb
Emile Papillon <emile.papillon@fcagroup.com>
2020-05-18 17:35:43 -0400

    Built app with signature

--- app/src/main/java/com/example/rosaccelpublisher/MainActivity.java
+++ app/src/main/java/com/example/rosaccelpublisher/MainActivity.java
@@ -1,21 +1,26 @@
 package com.example.rosaccelpublisher;
 
+import org.apache.commons.lang.NullArgumentException;
 import org.ros.address.InetAddressFactory;
 import org.ros.android.RosActivity;
-import org.ros.node.Node;
 import org.ros.node.NodeConfiguration;
 import org.ros.node.NodeMainExecutor;
 
+import java.util.EmptyStackException;
+
 public class MainActivity extends RosActivity
 {
-    protected MainActivity(String notificationTicker, String notificationTitle) {
-        super(notificationTicker, notificationTitle);
+    private AccelerationTalker aTalker;
+
+    public MainActivity()
+    {
+        super("Pubsub Tutorial", "Pubsub Tutorial");
     }
 
     @Override
     protected void init(NodeMainExecutor nodeMainExecutor)
     {
-        AccelerationTalker aTalker = new AccelerationTalker(this);
+        aTalker = new AccelerationTalker(this);
         NodeConfiguration nodeConfiguration = NodeConfiguration.newPublic(
                 InetAddressFactory.newNonLoopback().getHostAddress() );
         nodeConfiguration.setMasterUri(getMasterUri()) ;

0432722a9bfd2a66085f346d21db66dcf694753f
Emile Papillon <emile.papillon@fcagroup.com>
2020-05-19 07:50:58 -0400

    Changed acceleration to orientation

--- app/src/main/java/com/example/rosaccelpublisher/AccelerationListener.java
+++ app/src/main/java/com/example/rosaccelpublisher/AccelerationListener.java
@@ -14,22 +14,30 @@ public class AccelerationListener implements SensorEventListener {
         //
     }
 
-    private float[] acceleration = new float[]{0f, 0f, 0f};
+    private float[] orientation = new float[]{0f, 0f, 0f};
 
     @Override
     public void onSensorChanged(SensorEvent event)
     {
+        float[] mGravity = new float[3];
+        float[] mGeomagnetic = new float[3];
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+            mGravity = event.values;
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+            mGeomagnetic = event.values;
+        float R[] =  new float[9];
+        float I[] =  new float[9];
+        boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
+        if (success)
         {
-            acceleration[0] = event.values[0];
-            acceleration[1] = event.values[1];
-            acceleration[2] = event.values[2];
+            SensorManager.getOrientation(R, orientation);
         }
+
     }
 
     public float[] getSensorValue()
     {
-        return acceleration;
+        return orientation;
     }
 
     public AccelerationListener(Context c)
@@ -42,7 +50,9 @@ public class AccelerationListener implements SensorEventListener {
         SensorManager sm = (SensorManager)
                 context.getSystemService(Context.SENSOR_SERVICE);
         Sensor accel = sm.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0);
+        Sensor magnet = sm.getSensorList(Sensor.TYPE_MAGNETIC_FIELD).get(0); //added
         sm.registerListener(this, accel, SensorManager.SENSOR_DELAY_FASTEST);
+        sm.registerListener(this, magnet, SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     protected void onPause()
